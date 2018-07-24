2e2980e2e4fb638a41856605bd28e4c6d8755f44
Ashley Schultz <ashleyeschultz@gmail.com>
2018-07-24 01:24:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // Write your code in this file!
 const currentUser = 'ashley'
 
-const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!'
+let welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!'
 
 let excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + currentUser + '!'
-
